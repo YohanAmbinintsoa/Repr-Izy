3ad29fc575c9fa24ee7313b1e787ec52e2a9807e
YohanAmbinintsoa <yohanrabepro@gmail.com>
2024-01-25 23:48:54 +0300

    Update CategorieController.java

--- src/main/java/ITU/Baovola/Gucci/Controllers/CategorieController.java
+++ src/main/java/ITU/Baovola/Gucci/Controllers/CategorieController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/api/v1/Categories")
+@CrossOrigin
 public class CategorieController extends BaseController{
    
     @GetMapping
