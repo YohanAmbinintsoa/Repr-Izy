1203f24243c756129cf3a10faf083011dbcfded5
YohanAmbinintsoa <yohanrabepro@gmail.com>
2024-01-26 07:42:55 +0300

    Update JwtUtils.java

--- src/main/java/ITU/Baovola/Gucci/Security/JwtUtils.java
+++ src/main/java/ITU/Baovola/Gucci/Security/JwtUtils.java
@@ -21,8 +21,8 @@ public class JwtUtils {
 
   public static String getToken(HttpServletRequest request){
     String token="";
-    if (request.getParameter("authorization")!=null&&request.getParameter("authorization")!="") {
-        token=request.getParameter("authorization");
+    if (request.getHeader("authorization")!=null&&request.getHeader("authorization")!="") {
+        token=request.getHeader("authorization");
     }
     return token;
   }
