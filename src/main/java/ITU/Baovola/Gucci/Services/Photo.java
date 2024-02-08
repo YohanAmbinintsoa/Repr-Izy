7f77a05d6a29036edb6d8b937892ca2a5b8c2590
YohanAmbinintsoa <yohanrabepro@gmail.com>
2024-02-09 00:46:17 +0300

    Update Photo.java

--- src/main/java/ITU/Baovola/Gucci/Services/Photo.java
+++ src/main/java/ITU/Baovola/Gucci/Services/Photo.java
@@ -18,10 +18,10 @@ public class Photo {
         this.filename = filename;
     }
 
-    public File convertToFile() throws IOException {
+    public File convertToFile() throws Exception {
         Pattern pattern = Pattern.compile("data:image/(.*?);base64,");
         Matcher matcher = pattern.matcher(base64);
-        if (matcher.find()) {
+        matcher.find();
             String format = matcher.group(1);
             base64 = base64.substring(matcher.end());
             byte[] decodedBytes = Base64.getDecoder().decode(base64);
@@ -31,9 +31,7 @@ public class Photo {
             ImageIO.write(ImageIO.read(inputStream), format, outputImage);
             this.setFilename(filename+"."+format);
             return outputImage;
-        } else {
-            throw new IllegalArgumentException("Invalid base64 image format");
-        }
+        
     }
 
 
